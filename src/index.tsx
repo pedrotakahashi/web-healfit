1141765b5c20ac63e23ab1e62f602c81ce547d21
Pedro Takahashi <pedroh.takahashi@gmail.com>
2022-12-28 19:04:56 -0300

    add: Navbar base

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
