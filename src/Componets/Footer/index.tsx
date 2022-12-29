79ef360ebc93de7f6510fea1330b9e2661f8534f
Pedro Takahashi <pedroh.takahashi@gmail.com>
2022-12-29 00:43:08 -0300

    footer and header

--- src/Componets/Footer/index.tsx
+++ src/Componets/Footer/index.tsx
@@ -1,10 +1,18 @@
-const Footer = () =>{
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
-    return (
-        <>
-        <div className="div">isso é um Footer</div>
-        </>
-    )
+import './styles.css';
+
+const Footer = () =>{
+      return (
+        <Navbar bg="dark" expand="lg" className="d-flex align-itens-center " fixed="bottom">
+            <Container fluid>
+            <Navbar.Text className="text-footer">
+               HEAL FIT
+            </Navbar.Text>
+            </Container>
+      </Navbar>
+      );
 }
 
 export default Footer;
